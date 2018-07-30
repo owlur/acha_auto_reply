992219a40d0e1396adfa94a877fdd8e788f308d4
jaewoo <8864248@naver.com>
2018-07-31 00:13:49 +0900

    fix

--- session.py
+++ session.py
@@ -13,6 +13,7 @@ class Session:
 
     def receive_message(self, type, content):
         self.step += 1
+        print('user_key : ',self.user_key,'\ncontent : ',content)
         if type == 'text':
             for button_name in self.next.buttons:
                 if button_name == content and self.next.buttons[button_name]:
