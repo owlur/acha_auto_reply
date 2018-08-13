f1f59d3605f8e4629bf1686385eae54684cdec50
jaewoo <8864248@naver.com>
2018-08-13 19:54:30 +0900

    update alrim

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Message(Resource):
         content_parse = content.split('\n')
         if len(content_parse) > 2 and not content_parse[1] and content_parse[0] in setting.alrim_keyword:
             print('알림톡 응답 수신')
-            processing.parse_initial_reservation_alrim(content[2:])
+            processing.parse_initial_reservation_alrim('\n'.join(content_parse[2:]))
 
         if sessions.get(user_key):
             return sessions[user_key].receive_message(type, content)
