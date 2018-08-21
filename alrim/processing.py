8e75afb0ee7df76c4ea8ddeea22d091d30cec6bf
jaewoo <8864248@naver.com>
2018-08-21 21:27:19 +0900

    reserv_cancel

--- alrim/processing.py
+++ alrim/processing.py
@@ -3,11 +3,11 @@ from alrim import send
 from conversation import setting
 import DB
 
-template ={'FIRRM0006': "\[.+\]\n\n안녕하세요! .+ 님에게 아래와 같이 예약이 접수되었습니다.\n예약내용이 맞는지 확인 부탁드립니다!\n이름 : .+\n인원 : .+\n날짜 : .+\n\n예약 내용이 맞으실 경우 .+ 이내에 '확정' 버튼을, 아닐 경우 '취소' 버튼을 눌러주세요!\n'확정' 버튼을 누르실 경우 간편한 예약 관리 서비스 '아차'에 .+에 동의 하게 됩니다!\n이후 '아차' 플러스 친구를 통해 간편하게 예약 확인 및 취소가 가능합니다\n\n예약 번호 : [0-9]{16}"}
+template ={'FIRRM0006': "\[.+\]\n\n안녕하세요! .+ 님에게 아래와 같이 예약이 접수되었습니다.\n예약내용이 맞는지 확인 부탁드립니다!\n이름 : .+\n인원 : .+\n날짜 : .+\n\n예약 내용이 맞으실 경우 .+ 이내에 '확정' 버튼을, 아닐 경우 '취소' 버튼을 눌러주세요!\n\n'확정' 버튼을 누르실 경우 간편한 예약 관리 서비스 '아차'에 .+에 동의 하게 됩니다!\n\n이후 '아차' 플러스 친구를 통해 간편하게 예약 확인 및 취소가 가능합니다\n\n예약 번호 : [0-9]{16}"}
 
 
 def reserv_regist(phone_number, store_name, person_name, person_num, date, token):
-    template_code = 'FIRRM0005'
+    template_code = 'FIRRM0006'
     template_parameter = {'상호명': store_name, '이름': person_name, '인원': person_num, '날짜': date, '예약번호': token, '제한시간': '30분', \
                           '법률': '개인정보의 제3자 수집 이용 제공', 'mobile_url': 'https://www.privacy.go.kr/a3sc/per/chk/examInfoViewFQ41.do', \
                           'pc_url': 'https://www.privacy.go.kr/a3sc/per/chk/examInfoViewFQ41.do'}
