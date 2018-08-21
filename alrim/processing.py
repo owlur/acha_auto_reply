efc036b1e002cb4b6903b6b501b6db239b01ae73
jaewoo <8864248@naver.com>
2018-08-21 21:32:15 +0900

    reserv_cancel

--- alrim/processing.py
+++ alrim/processing.py
@@ -23,6 +23,8 @@ def parse_initial_reservation_alrim(session, command, content):
         print(content,'\n 템플릿 일치하지 않음')
         return False
     splited_content = content.split('\n')
+    print(content)
+    print(splited_content)
 
     store_name = splited_content[0][1:-1]
     person_name = splited_content[4].split('이름 : ')[1]
