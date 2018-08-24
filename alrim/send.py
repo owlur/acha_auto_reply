9748f30a5bf9aa46f6c7842929ff37b11655aee4
jaewoo <8864248@naver.com>
2018-08-24 14:48:01 +0900

    get today alrim list

--- alrim/send.py
+++ alrim/send.py
@@ -10,8 +10,12 @@ header = {'X-Secret-Key': secret, 'Content-Type': 'application/json;charset=UTF-
 plusFriendId = 'ah_cha'
 
 
-def send_interval_alrim(phone_number, store_name, person_name, person_num, date, token):
+def send_interval_alrim(phone_number, store_name, person_name, person_num, date, until_date, address, token):
     template_code = 'RRM004'
+    template_parameter = {'상호명': store_name, '이름': person_name, '인원': person_num, '날짜': date, '예약번호': token, '남은일수': until_date // 1440, \
+                          'mobile_url': 'http://api.acha.io:3000/user/map?addr=%s' % address, \
+                          'pc_url': 'http://api.acha.io:3000/user/map?addr=%s' % address}
+    return send_alrim(template_code, phone_number, template_parameter)
 
 
 def send_alrim(template_code, phone_number, template_parameter):
