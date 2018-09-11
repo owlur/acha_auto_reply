29e8668874bb95ea87aa8bba6a22c25383314942
jaewoo <8864248@naver.com>
2018-09-11 16:03:50 +0900

    update interval send

--- utils.py
+++ utils.py
@@ -3,7 +3,15 @@ from datetime import datetime
 
 def datetime2str(origin_datetime):
     now = datetime.now()
-    result = '%d월 %d일 %d시' % (origin_datetime.month, origin_datetime.day, origin_datetime.hour)
+    if origin_datetime.hour > 12:
+        hour = '오후 %d시' % (origin_datetime.hour - 12)
+    elif origin_datetime.hour == 12:
+        hour = '점심 12시'
+    elif origin_datetime.hour == 0:
+        hour = '밤 12시'
+    else:
+        hour = '오전 %d시' % origin_datetime
+    result = '%d월 %d일 %s' % (origin_datetime.month, origin_datetime.day, hour)
 
     if origin_datetime.year > now.year:
         result_year = '%d년 ' % origin_datetime.year
