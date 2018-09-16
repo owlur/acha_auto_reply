35d45f2c8d6d54e10f848d149b0ba928a74f113b
jaewoo <8864248@naver.com>
2018-09-17 01:23:03 +0900

    update auto cancel

--- main.py
+++ main.py
@@ -105,15 +105,10 @@ class Message(Resource):
             res = processing.alrim_response_parsing(sessions[user_key], content_parse[0],
                                                     '\n'.join(content_parse[2:]))
 
-            print('check', res)
             if res[1] == '확정':
-                print('check2')
                 for reserv in regist_queue:
-                    print('check3', reserv)
                     if reserv[0] == res[2]:
-                        print('check4', regist_queue)
                         regist_queue.remove(reserv)
-                        print('check5', regist_queue)
                         break
                 else:
                     print('예약등록큐에 존재하지 않는 예약번호 입니다. %s', res[2])
@@ -183,7 +178,6 @@ api.add_resource(PrivacyPolicy, '/PrivacyPolicy')
 
 def check_regist():
     start = time.time()
-    print(regist_queue)
     while regist_queue and regist_queue[0][1] < datetime.now():
         reserv_id = regist_queue.popleft()[0]
         res = DB.reservation_cancel(reserv_id)
