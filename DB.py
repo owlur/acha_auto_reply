a5433f51bfa33f2091be7e1086c6c6e0d7978e55
jaewoo <8864248@naver.com>
2018-10-03 13:32:33 +0900

    phone dict

--- DB.py
+++ DB.py
@@ -214,7 +214,8 @@ def set_name(reserv_id, person_name):
 
 def get_current_status(reserv_id='', token=''):
     params = {'key': API_KEY, 'reservId': reserv_id, 'token': token}
-    res = requests.get(base_url + '/reserv/getstatus', params)
+    #res = requests.get(base_url + '/reserv/getstatus', params)
+    res = requests.get(base_url + '/reserv/currentstatus', params)
     print(res.text)
     return res.json()
 
