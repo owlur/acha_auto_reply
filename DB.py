e2beda8c73735e8649c97d4ce39309a27002d6d6
jaewoo <8864248@naver.com>
2018-10-07 13:03:39 +0900

    real server

--- DB.py
+++ DB.py
@@ -263,7 +263,10 @@ def set_name(reserv_id, person_name):
 def get_current_status(reserv_id='', token=''):
     params = {'key': API_KEY, 'reservId': reserv_id, 'reservToken': token}
     #res = requests.get(base_url + '/reserv/getstatus', params)
-    res = requests.get(base_url + '/reserv/currentstatus', params)
+    if utils.is_test():
+        res = requests.get(base_url + '/reserv/getstatus', params)
+    else:
+        res = requests.get(base_url + '/reserv/currentstatus', params)
     print(res.text)
     return res.json()
 
